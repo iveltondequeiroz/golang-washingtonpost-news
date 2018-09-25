75432d3d2a3e61fb126a9bcc66c5998923ed9328
@ivelton2018 <ivelton2018@gmail.com>
2018-09-24 20:57:10 -0400

    server/xml parser

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
 
-func main(){
-  fmt.Println("News Aggregator")
+type Sitemapindex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
+
+type NewsAggPage struct {
+	Title string
+	News  map[string]NewsMap
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+
+	var s Sitemapindex
+	var n News
+	news_map := make(map[string]NewsMap)
+
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+		for idx, _ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	p := NewsAggPage{Title: "Amazing News", News: news_map}
+	t, _ := template.ParseFiles("basictemplating.html")
+	fmt.Println(t.Execute(w, p))
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", newsAggHandler)
+	http.ListenAndServe(":8000", nil)
 }
